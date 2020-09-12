842f502b22175d4d445372398635fc353aee5a1b
Everspace <7560635+Everspace@users.noreply.github.com>
2020-09-11 21:00:44 -0700

    Dont need "commit changes", since it works fine.

--- src/commands/commitChanges.ts
+++
@@ -1,9 +0,0 @@
-
-export const stageChanges = vscode.commands.registerCommand(
-  "rightclick-git.stageChanges",
-  async (_, allSelected: vscode.Uri[]) => {
-    const { files, directories } = await sortFilesAndDirs(...allSelected)
-
-    const allFiles = await Promise.all(directories.map(getAllFiles))
-    allFiles.push(files)
-    const fileList: vscode.Uri[] = allFiles.reduce(concat, [])
